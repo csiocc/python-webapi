53686176c671cfdf063929108d3483dc28f33a24
Christian <christian.siegrist@open-circle.ch>
2025-10-03 10:15:10 +0200

    fix mh score route

--- main.py
+++ main.py
@@ -71,11 +71,12 @@ def update_player_score(player_id: int, score_update: schemas.PlayerUpdateScore,
     return player
 
 @app.put("/players/{player_id}/mhscore", response_model=schemas.Player)
-def update_mhscore(player_id: int, new_score: int, db: Session = Depends(get_db)):
-    player = crud.update_mhscore(db, player_id, new_score)
-    if not player:
-        raise HTTPException(status_code=404, detail="Player not found")
-    return player
+def update_player_mhscore(
+    player_id: int,
+    score_update: schemas.PlayerUpdateMhScore,
+    db: Session = Depends(get_db)
+):
+    return crud.update_player_mhscore(db, player_id, score_update)
 
 #  Login
 @app.post("/login/", response_model=schemas.Player)
